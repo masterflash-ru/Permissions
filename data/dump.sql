c21625acda3ffdb82795b5216e06acee04bf8909
Masterflash <sxq@yandex.ru>
2018-02-28 09:44:39 +0300

    f sql

--- data/dump.sql
+++ data/dump.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.6.39, for FreeBSD11.1 (i386)
 --
--- Host: localhost    Database: test
+-- Host: localhost    Database: simba4
 -- ------------------------------------------------------
 -- Server version	5.6.39
 
@@ -15,113 +15,13 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `permission`
---
 
-DROP TABLE IF EXISTS `permission`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `permission` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` char(127) DEFAULT NULL,
-  `description` char(255) DEFAULT NULL,
-  `date_created` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='доступы';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `permission`
---
-
-LOCK TABLES `permission` WRITE;
-/*!40000 ALTER TABLE `permission` DISABLE KEYS */;
-INSERT INTO `permission` VALUES (1,'admin.login','Вход в админку','2017-09-22 13:15:49'),(3,'admin.manage','Управление администраторами','2017-09-23 07:02:03'),(4,'permission.manage','Управление привелегиями','2017-09-23 07:02:03'),(5,'role.manage','Управление ролями','2017-09-23 07:02:03'),(6,'profile.any.view','Управление любыми профилями','2017-09-23 07:02:03'),(7,'profile.own.view','Управление своим профилем','2017-09-23 07:02:03');
-/*!40000 ALTER TABLE `permission` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `role`
---
-
-DROP TABLE IF EXISTS `role`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `role` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` char(127) DEFAULT NULL,
-  `description` char(255) DEFAULT NULL,
-  `date_created` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='роли';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `role`
---
-
-LOCK TABLES `role` WRITE;
-/*!40000 ALTER TABLE `role` DISABLE KEYS */;
-INSERT INTO `role` VALUES (1,'admin','Администратор','2017-09-22 13:14:39'),(2,'guest','Гостевой вход','2017-09-22 13:16:53');
-/*!40000 ALTER TABLE `role` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `role2permission`
---
-
-DROP TABLE IF EXISTS `role2permission`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `role2permission` (
-  `role` int(11) NOT NULL,
-  `permission` int(11) NOT NULL,
-  `code` int(11) DEFAULT NULL COMMENT 'битовая структура доступа',
-  PRIMARY KEY (`role`,`permission`),
-  KEY `permission` (`permission`),
-  KEY `role` (`role`),
-  CONSTRAINT `role2permission_fk` FOREIGN KEY (`permission`) REFERENCES `permission` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `role2permission_fk1` FOREIGN KEY (`role`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='роль-доступ';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `role2permission`
---
-
-LOCK TABLES `role2permission` WRITE;
-/*!40000 ALTER TABLE `role2permission` DISABLE KEYS */;
-INSERT INTO `role2permission` VALUES (1,1,NULL);
-/*!40000 ALTER TABLE `role2permission` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `role_tree`
---
 
 DROP TABLE IF EXISTS `role_tree`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `role_tree` (
-  `role` int(11) NOT NULL,
-  `parent_role` int(11) NOT NULL,
-  PRIMARY KEY (`role`,`parent_role`),
-  KEY `role` (`role`),
-  KEY `parent_role` (`parent_role`),
-  CONSTRAINT `role_tree_fk` FOREIGN KEY (`role`) REFERENCES `role` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `role_tree_fk1` FOREIGN KEY (`parent_role`) REFERENCES `role` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `role_tree`
---
-
-LOCK TABLES `role_tree` WRITE;
-/*!40000 ALTER TABLE `role_tree` DISABLE KEYS */;
-/*!40000 ALTER TABLE `role_tree` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `role2permission`;
+DROP TABLE IF EXISTS `users2role`;
+DROP TABLE IF EXISTS `role`;
+DROP TABLE IF EXISTS `permission`;
 
 --
 -- Table structure for table `users`
@@ -162,59 +62,105 @@ INSERT INTO `users` VALUES (1,'root',1,'$2y$10$TryLBUTSX7lZdSD8NBUFMOu8.vzvfoqaF
 UNLOCK TABLES;
 
 --
--- Table structure for table `users2permission`
+-- Table structure for table `users2group`
 --
 
-DROP TABLE IF EXISTS `users2permission`;
+DROP TABLE IF EXISTS `users2group`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users2permission` (
+CREATE TABLE `users2group` (
   `users` int(11) NOT NULL,
-  `permission` int(11) NOT NULL,
-  `code` int(11) DEFAULT NULL COMMENT 'Битовая структура доступа',
-  PRIMARY KEY (`users`,`permission`),
+  `users_group` int(11) NOT NULL,
+  PRIMARY KEY (`users`,`users_group`),
   KEY `users` (`users`),
-  KEY `permission` (`permission`),
-  CONSTRAINT `users2permission_fk` FOREIGN KEY (`users`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `users2permission_fk1` FOREIGN KEY (`permission`) REFERENCES `permission` (`id`) ON DELETE CASCADE
+  KEY `users_group` (`users_group`),
+  CONSTRAINT `users2group_fk` FOREIGN KEY (`users`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `users2group_fk1` FOREIGN KEY (`users_group`) REFERENCES `users_group` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='привязка юзер-группа';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users2group`
+--
+
+LOCK TABLES `users2group` WRITE;
+/*!40000 ALTER TABLE `users2group` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users2group` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users_ext`
+--
+
+DROP TABLE IF EXISTS `users_ext`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_ext` (
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `users_ext_fk` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='расширение юзеров';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_ext`
+--
+
+LOCK TABLES `users_ext` WRITE;
+/*!40000 ALTER TABLE `users_ext` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users_ext` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users_group`
+--
+
+DROP TABLE IF EXISTS `users_group`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` char(127) DEFAULT NULL,
+  `description` char(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COMMENT='роли';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_group`
+--
+
+LOCK TABLES `users_group` WRITE;
+/*!40000 ALTER TABLE `users_group` DISABLE KEYS */;
+INSERT INTO `users_group` VALUES (1,'Группа администраторы','Администратор - полный доступ ко всем ресурсам'),(2,'Группа редакторы','Редакторы информации'),(6,'Группа регистрированные посетители','Регистрированные посетители'),(9,'Группа гости','Гостевой вход - группа с минимальным уровнем доступа');
+/*!40000 ALTER TABLE `users_group` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users_group_tree`
+--
+
+DROP TABLE IF EXISTS `users_group_tree`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_group_tree` (
+  `id` int(11) NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`parent_id`),
+  KEY `role` (`id`),
+  KEY `parent_role` (`parent_id`),
+  CONSTRAINT `users_group_tree_fk` FOREIGN KEY (`id`) REFERENCES `users_group` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `users_group_tree_fk1` FOREIGN KEY (`parent_id`) REFERENCES `users_group` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `users2permission`
+-- Dumping data for table `users_group_tree`
 --
 
-LOCK TABLES `users2permission` WRITE;
-/*!40000 ALTER TABLE `users2permission` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users2permission` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users2role`
---
-
-DROP TABLE IF EXISTS `users2role`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users2role` (
-  `users` int(11) NOT NULL,
-  `role` int(11) NOT NULL,
-  PRIMARY KEY (`users`,`role`),
-  KEY `users` (`users`),
-  KEY `role` (`role`),
-  CONSTRAINT `users2role_fk` FOREIGN KEY (`users`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `users2role_fk1` FOREIGN KEY (`role`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='юзер-роль';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users2role`
---
-
-LOCK TABLES `users2role` WRITE;
-/*!40000 ALTER TABLE `users2role` DISABLE KEYS */;
-INSERT INTO `users2role` VALUES (1,1);
-/*!40000 ALTER TABLE `users2role` ENABLE KEYS */;
+LOCK TABLES `users_group_tree` WRITE;
+/*!40000 ALTER TABLE `users_group_tree` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users_group_tree` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -226,4 +172,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-02-13  8:35:26
+-- Dump completed on 2018-02-28  9:38:56
